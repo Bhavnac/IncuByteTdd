ae6ff09e6077d8815b5731cdbff8f07dfadd9625
Bhavnac <bhavna9295@gmail.com>
2024-04-23 22:29:43 +0530

    Adding test case when input is adding new line between numbers

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -20,5 +20,11 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1,2")).to eq(3)
       end
     end
+
+    context "when input includes new lines between numbers" do
+      it "returns their sum" do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+      end
+    end
   end
 end
