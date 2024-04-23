35c2fa315f054ef829c5470e00ef7228fc1a9a7f
Bhavnac <bhavna9295@gmail.com>
2024-04-23 22:30:27 +0530

    Adding test case for custom delimeter and negative numbers

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -26,5 +26,18 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1\n2,3")).to eq(6)
       end
     end
+    
+    context "when input includes custom delimiter" do
+      it "returns their sum using the custom delimiter" do
+        expect(StringCalculator.add("//;\n1;2")).to eq(3)
+        expect(StringCalculator.add("//[***]\n1***2***3")).to eq(6)
+      end
+    end
+
+    context "when input includes negative numbers" do
+      it "raises an exception for negative numbers" do
+        expect { StringCalculator.add("1,-2,3,-4") }.to raise_error(RuntimeError, "negative numbers not allowed: -2, -4")
+      end
+    end
   end
 end
